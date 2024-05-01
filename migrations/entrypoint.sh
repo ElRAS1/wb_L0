6f424f0c74d2c2f2db5eab514427a73f6a54501a
Elmir <elmir.rasulov.2002@mail.ru>
2024-05-01 22:43:56 +0300

    final

--- migrations/entrypoint.sh
+++ migrations/entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-ls /migrations
-# migrate -path=/migrations -database=postgresql://wb:wb@postgres:5432/wb?sslmode=disable up
-# migrate -path=/migrations -database=postgresql://wb:wb@postgres:5432/wb?sslmode=disable down --all
 migrate -path=/migrations -database=postgresql://wb:wb@postgres:5432/wb?sslmode=disable up
 
